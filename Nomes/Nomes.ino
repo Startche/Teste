1a127c1c50dca8886f844a89bae15ac44fd4ef88
Luiz Felipe <lfm@ufrgs.br>
2023-05-29 15:57:24 -0300

    Update Nomes.ino

--- Nomes/Nomes.ino
+++ Nomes/Nomes.ino
@@ -4,15 +4,9 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Nomes abaixo");
-  Serial.println("Gabriel F.");
-  Serial.println("Laurópatra");
-  Serial.println("Caio Vinicius");
-  Serial.println("Luma Gomes");
-  Serial.println("Pedro Arthur");
-  Serial.println("Gabriela");
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Luiz");
 }
